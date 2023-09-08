481091678c064a415141f719206331e006a73d8b
PANZEBIAO <e181117@eiga.ac.jp>
2023-09-08 17:19:49 +0900

    0908_blogindexedit_han

---
+++ js/blog_edit.js
@@ -0,0 +1,17 @@
+const imageUpload = document.getElementById("imageUpload");
+const imagePreview = document.getElementById("imagePreview");
+
+imageUpload.addEventListener("change", function () {
+    const file = this.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            imagePreview.src = e.target.result;
+            imagePreview.style.display = "block";
+        };
+
+        reader.readAsDataURL(file);
+    }
+});
